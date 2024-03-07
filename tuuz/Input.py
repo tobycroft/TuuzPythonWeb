5605d45b84a9499ec0d31a0cda3ca7228e8eed14
tuuz <silverhawk@foxmail.com>
2024-03-07 23:12:00 +0800

    TuuzPC

--- tuuz/Input.py
+++ tuuz/Input.py
@@ -14,6 +14,38 @@ class Post:
             if need_xss:
                 # TODO: 需要完成xss过滤这里
                 out = (in_data)
-                return out, True
+                return out
             else:
-                return in_data, True
+                return in_data
+
+
+class Get:
+    @staticmethod
+    def String(key, need_xss=False):
+        in_data = request.args.get(key)
+        if not in_data:
+            flask.abort(make_response(tuuz.Ret.fail(400, echo="GET-[" + key + "]")))
+        else:
+            if need_xss:
+                # TODO: 需要完成xss过滤这里
+                out = (in_data)
+                return out
+            else:
+                return in_data
+
+
+class Combi:
+    @staticmethod
+    def String(key, need_xss=False):
+        in_data = request.form.get(key)
+        if not in_data:
+            in_data = request.args.get(key)
+        if not in_data:
+            flask.abort(make_response(tuuz.Ret.fail(400, echo="COMBI-[" + key + "]")))
+        else:
+            if need_xss:
+                # TODO: 需要完成xss过滤这里
+                out = (in_data)
+                return out
+            else:
+                return in_data
